57dc52a2306496d22caf37a6aa0efcef1b37bb0b
AAAMON <andreeapetcu@protonmail.com>
2025-03-03 21:55:59 +0200

    Fix t1p1

--- tema1/t1p1.cpp
+++ tema1/t1p1.cpp
@@ -12,15 +12,20 @@ int main ()
 	std::cout << "Hello World\n";
 
 	int m = 0;
+	float prev_u = -1;
 	float u = 1;
 
 	// MAIN LOOP
-	while (!(1 + u = 1))
+	while (!(1 + u == 1))
 	{
 		++m;
+		prev_u = u;
 		update_u(u);
 		std::cout << "u is: " << u << '\n';
 	}
+
+	std::cout << "Precizia masina e: " << prev_u << '\n';
+	// resulting u is 1.4013e-45
 
 	return 0;
 }
