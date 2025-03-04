614b2f34b5d9e1d4af610da7f0191ebef5aceb34
Nistor Amalia-Cristina <124942722+n-amalia@users.noreply.github.com>
2025-03-04 17:24:32 +0200

    corrected result
    we used prev_u before, but this was the last number that DID NOT satisfy the condition. we needed the first one that DID satisfy the condition

--- tema1/t1p1.cpp
+++ tema1/t1p1.cpp
@@ -2,12 +2,10 @@
 
 #include <iostream>
 
-
 void update_u(float& u) // u is of form: 10^(-m)
 {
 	u = u/10.0;
 }
-
 
 int main ()
 {
@@ -24,9 +22,9 @@ int main ()
 		std::cout << "u este: " << u << '\n';
 	}
 
-	std::cout << "Precizia masina e: " << prev_u << '\n';
-	// Andy: resulting u is 1.4013e-45
-	// Ama: u = 1e-07
+	std::cout << "Precizia masina e: " << u << '\n';
+	// Andy: resulting u is 1.4013e-45 (before correction)
+	// Ama: u = 1e-08
 
 	return 0;
 }
