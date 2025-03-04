4b26e0a9e9b388b5d767d392358b6e2fda121b97
Nistor Amalia-Cristina <124942722+n-amalia@users.noreply.github.com>
2025-03-04 17:25:08 +0200

    completed first task

--- tema1/t1p2.cpp
+++ tema1/t1p2.cpp
@@ -1,17 +1,18 @@
+// Task: Check associativity based on results from previous exercise.
+// Task: Find a set of numbers where the multiplication is not associative.
+
 #include <iostream>
 
 int main ()
 {
-	std::cout << "Hello World\n";
-
-	// we took this value from the previous exercise. See file t1p1.cpp
-	float u = 1e-07;
+	// we took this value from the previous exercise (see file t1p1.cpp)
+	float u = 1e-08;
 
 	float x = 1.0;
 	float y = u/10.0;
 	float z = u/10.0;
 
-	std::cout << "Relatia (x+y)+z != x+(y+z) este evaluata ca: " << ((x+y)+z != x + (y+z));
+	std::cout << "Relatia (x + y) + z != x + (y + z) este evaluata ca: " << ((x + y) + z != x + (y + z)) << '\n';
 
 	return 0;
 }
