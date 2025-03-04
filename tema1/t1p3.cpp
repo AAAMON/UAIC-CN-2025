f211230f88ca9e8454df8aa29e096a710cc9018f
AAAMON <andreeapetcu@protonmail.com>
2025-03-04 20:18:19 +0200

    t1p3: Calculate polynoms faster

--- tema1/t1p3.cpp
+++ tema1/t1p3.cpp
@@ -17,69 +17,39 @@ double c4 = 2.7557319223985890652557319223986e-6;
 double c5 = 2.5052108385441718775052108385442e-8;
 double c6 = 1.6059043836821614599392377170155e-10;
 
-double P1(double x)
-{
-    double y = x * x;
-    return x * (1 + y * (- c1 + c2 * y));
-}
-
-double P2(double x)
-{
-    double y = x * x;
-    return x * (1 + y * (- c1 + y * (c2 - c3 * y)));
-}
-
-double P3(double x)
-{
-    double y = x * x;
-    return x * (1 + y * (- c1 + y * (c2 + y * (- c3  + c4 * y))));
-}
-
-double P4(double x)
-{
-    double y = x * x;
-    return x * (1 + y * (-0.166 + y * (0.00833 + y * (- c3  + c4 * y))));
-}
-
-double P5(double x)
-{
-    double y = x * x;
-    return x * (1 + y * (-0.1666 + y * (0.008333 + y * (- c3  + c4 * y))));
-}
-
-double P6(double x)
-{
-    double y = x * x;
-    return x * (1 + y * (-0.16666 + y * (0.0083333 + y * (- c3  + c4 * y))));
-}
-
-double P7(double x)
-{
-    double y = x * x;
-    return x * (1 + y * (- c1 + y * (c2 + y * (- c3  + y * (c4 - c5 * y)))));
-}
-
-double P8(double x)
-{
-    double y = x * x;
-    return x * (1 + y * (- c1 + y * (c2 + y * (- c3  + y * (c4 + y * (- c5 + c6 * y))))));
-}
 
 void calculeaza_polinoame_si_scrie_top3(std::ofstream& file, double victim)
 {
     double correct_val = std::sin(victim);
 
+    double help1 = victim * victim;
+    // help1 e x^2
+    double P1 = victim * (1 + help1 * (- c1 + c2 * help1));
+    double help2 = help1 * help1 * help1 * victim;
+    // help2 e x^7
+    double P2 = P1 - c3*help2;
+    double help3 = -c3*help2 + c4*help2*help1;
+    // help3 e -c3*x^7 + c4*x^9
+    double P3 = P1 + help3;
+    double P4 = victim * (1 + help1 * (-0.166 + help1 * 0.00833)) + help3;
+    double P5 = victim * (1 + help1 * (-0.1666 + help1 * 0.008333)) + help3;
+    double P6 = victim * (1 + help1 * (-0.16666 + help1 * 0.0083333)) + help3;
+    double help4 = help2*help1*help1;
+    // help4 e x^11
+    double P7 = P3 - c5*help4;
+    double P8 = P7 + c6*help4*help1;
+
     // calculam direct si erorile
     std::vector<std::pair<double, std::string>> polinoamee =
     {
-	{std::abs(P1(victim) - correct_val), "1"},
-	{std::abs(P2(victim) - correct_val), "2"},
-	{std::abs(P3(victim) - correct_val), "3"},
-	{std::abs(P4(victim) - correct_val), "4"},
-	{std::abs(P5(victim) - correct_val), "5"},
-	{std::abs(P6(victim) - correct_val), "6"},
-	{std::abs(P7(victim) - correct_val), "7"},
-	{std::abs(P8(victim) - correct_val), "8"},
+	{std::abs(P1 - correct_val), "1"},
+	{std::abs(P2 - correct_val), "2"},
+	{std::abs(P3 - correct_val), "3"},
+	{std::abs(P4 - correct_val), "4"},
+	{std::abs(P5 - correct_val), "5"},
+	{std::abs(P6 - correct_val), "6"},
+	{std::abs(P7 - correct_val), "7"},
+	{std::abs(P8 - correct_val), "8"},
     };
     std::sort(polinoamee.begin(), polinoamee.end());
     
