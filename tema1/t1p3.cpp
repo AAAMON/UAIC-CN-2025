9ef693f9fc5501a5f887ef06cc6a06cee12e49dc
Nistor Amalia-Cristina <124942722+n-amalia@users.noreply.github.com>
2025-03-09 19:07:26 +0200

    added bonus (timers)

--- tema1/t1p3.cpp
+++ tema1/t1p3.cpp
@@ -20,26 +20,70 @@ double c4 = 2.7557319223985890652557319223986e-6;
 double c5 = 2.5052108385441718775052108385442e-8;
 double c6 = 1.6059043836821614599392377170155e-10;
 
-void calculeaza_polinoame_si_scrie_top3(std::ofstream &file, double victim)
+void calculeaza_polinoame_si_scrie_top3(std::ofstream &file, double victim, std::vector<int> times)
 {
     double correct_val = std::sin(victim);
 
     double help1 = victim * victim;
     // help1 e x^2
+
+    using namespace std::chrono;
+    auto start = high_resolution_clock::now();
     double P1 = victim * (1 + help1 * (-c1 + c2 * help1));
+    auto end = high_resolution_clock::now();
+    auto duration = duration_cast<milliseconds>(end - start).count();
+    times[0] = duration;
+
     double help2 = help1 * help1 * help1 * victim;
     // help2 e x^7
+
+    start = high_resolution_clock::now();
     double P2 = P1 - c3 * help2;
+    end = high_resolution_clock::now();
+    duration = duration_cast<milliseconds>(end-start).count();
+    times[1] = duration;
+
     double help3 = -c3 * help2 + c4 * help2 * help1;
     // help3 e -c3*x^7 + c4*x^9
+
+    start = high_resolution_clock::now();
     double P3 = P1 + help3;
+    end = high_resolution_clock::now();
+    duration = duration_cast<milliseconds>(end-start).count();
+    times[2] = duration;
+
+    start = high_resolution_clock::now();
     double P4 = victim * (1 + help1 * (-0.166 + help1 * 0.00833)) + help3;
+    end = high_resolution_clock::now();
+    duration = duration_cast<milliseconds>(end-start).count();
+    times[3] = duration;
+
+    start = high_resolution_clock::now();
     double P5 = victim * (1 + help1 * (-0.1666 + help1 * 0.008333)) + help3;
+    end = high_resolution_clock::now();
+    duration = duration_cast<milliseconds>(end-start).count();
+    times[4] = duration;
+
+    start = high_resolution_clock::now();
     double P6 = victim * (1 + help1 * (-0.16666 + help1 * 0.0083333)) + help3;
+    end = high_resolution_clock::now();
+    duration = duration_cast<milliseconds>(end-start).count();
+    times[5] = duration;
+
     double help4 = help2 * help1 * help1;
     // help4 e x^11
+
+    start = high_resolution_clock::now();
     double P7 = P3 - c5 * help4;
+    end = high_resolution_clock::now();
+    duration = duration_cast<milliseconds>(end-start).count();
+    times[6] = duration;
+
+    start = high_resolution_clock::now();
     double P8 = P7 + c6 * help4 * help1;
+    end = high_resolution_clock::now();
+    duration = duration_cast<milliseconds>(end-start).count();
+    times[7] = duration;
 
     // calculam direct si erorile
     std::vector<std::pair<double, std::string>> polinoamee =
@@ -83,14 +127,40 @@ int main()
 
     using namespace std::chrono;
     auto start = high_resolution_clock::now();
+
+    std::vector<int> times = {0, 0, 0, 0, 0, 0, 0, 0};
+    std::vector<std::pair<int, std::string>> times_median =
+    {
+        {0, "1"}, 
+        {0, "2"}, 
+        {0, "3"}, 
+        {0, "4"}, 
+        {0, "5"}, 
+        {0, "6"}, 
+        {0, "7"}, 
+        {0, "8"}
+    };    
+
     // MAIN LOOP
     for (int i = 1; i <= NUMBERS_COUNT; ++i)
     {
         // generam random folosind distributia dist() cu generatorul gen
         current_victim = dist(gen);
         std::cout << "#" << i << ": Number: " << current_victim << '\n';
-        calculeaza_polinoame_si_scrie_top3(file, current_victim);
+        calculeaza_polinoame_si_scrie_top3(file, current_victim, times);
+
+        for(int j = 0; j < 8; j++)
+        {
+            std::cout<<"Polinomul #"<<j<<": "<<times[j]<<"(ms)\n";
+            times_median[j].first += times[j];
+        }
+        std::cout<<"----------\n";
     }
+    for(int i = 0; i < 8; i++)
+    {
+        times_median[i].first /= NUMBERS_COUNT;
+    }
+    std::sort(times_median.begin(), times_median.end());
 
     if (!infile)
     {
@@ -132,6 +202,11 @@ int main()
     for(int i = 0; i < 8; i++)
     {
         std::cout << "#" << i + 1 << ": " << scores[i].second << "(scor: " << scores[i].first << ") \n";
+    }
+    std::cout<<"Timpii de lucru: \n";
+    for(int i = 0; i < 8; i++)
+    {
+        std::cout << "#" << i + 1 << ": " << times_median[i].first << "(ms) \n";
     }
 
     auto end = high_resolution_clock::now();
