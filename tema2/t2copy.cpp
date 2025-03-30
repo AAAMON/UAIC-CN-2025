1c8933a0026cf10b1906e153031bc58e2adbce01
Nistor Amalia-Cristina <amalia.nistor718@gmail.com>
2025-03-30 10:39:38 +0300

    t2: added bonus

--- tema2/t2copy.cpp
+++ tema2/t2copy.cpp
@@ -216,6 +216,126 @@ void calculEigen(const vector<vector<double>> &Ainit, vector<double> &b, vector<
     cout << "||xLU - Alib_inv * b||2 = " << norm2 << "\n";
 }
 
+// BONUS ///////////////////////////////////////////////////////////////////////////////////////
+double getL(const vector<double> &L, int i, int j, int n)
+{
+    if (i >= j) // lower triangular elements (includes diagonal)
+        return L[i * (i + 1) / 2 + j];
+    return 0.0; // Upper part is zero in L
+}
+
+double getU(const vector<double> &U, const vector<double> &dU, int i, int j, int n)
+{
+    if (i == j)
+        return dU[i]; // Diagonal stored separately
+    if (i < j)        // upper triangular elements
+        return U[i * n + j - (i * (i + 1) / 2 + (i + 1))];
+    return 0.0; // Lower part is zero in U
+}
+
+void LUdecomposition(const vector<vector<double>> &A, vector<double> &L, vector<double> &U, vector<double> &dU, int n, double epsilon)
+{
+    for (int p = 0; p < n; ++p)
+    {
+        for (int i = p; i < n; ++i) // Compute column `p` of L
+        {
+            double sum = 0.0;
+            for (int z = 0; z < p; ++z)
+            {
+                sum += getL(L, i, z, n) * getU(U, dU, z, p, n);
+            }
+
+            // L[i][p] = (A[i][p] - sum) / U[p][p]
+            if (!isZero(dU[p], epsilon))
+            {
+                L[i * (i + 1) / 2 + p] = (A[i][p] - sum) / dU[p];
+            }
+            else
+            {
+                abortt = true;
+                return;
+            }
+        }
+
+        for (int i = p + 1; i < n; ++i) // Compute row `p` of U
+        {
+            double sum = 0.0;
+            for (int z = 0; z < p; ++z)
+            {
+                sum += getL(L, p, z, n) * getU(U, dU, z, i, n);
+            }
+
+            // U[p][j] = (A[p][j] - sum) / L[p][p]
+            if (!isZero(L[p * (p + 1) / 2 + p], epsilon))
+            {
+                U[p * n + i - (p * (p + 1) / 2 + (p + 1))] = (A[p][i] - sum) / L[p * (p + 1) / 2 + p];
+            }
+            else
+            {
+                abortt = true;
+                return;
+            }
+        }
+    }
+}
+
+vector<double> solveLyB(const vector<double> &L, const vector<double> &b, int n, double epsilon) // substitutie directa
+{
+    vector<double> y(n);
+    for (int i = 0; i < n; i++)
+    {
+        double sum = 0.0;
+        for (int j = 0; j < i; j++)
+            sum += getL(L, i, j, n) * y[j];
+
+        if (!isZero(getL(L, i, i, n), epsilon))
+        {
+            y[i] = (b[i] - sum) / getL(L, i, i, n);
+        }
+        else
+        {
+            abortt = true;
+            return y;
+        }
+    }
+    return y;
+}
+
+vector<double> solveUxY(const vector<double> &U, const vector<double> &dU, const vector<double> &y, int n, double epsilon) // substitutie inversa
+{
+    vector<double> x(n);
+    for (int i = n - 1; i >= 0; i--)
+    {
+        double sum = 0.0;
+        for (int j = i + 1; j < n; j++)
+            sum += getU(U, dU, i, j, n) * x[j];
+
+        if (!isZero(dU[i], epsilon))
+        {
+            x[i] = (y[i] - sum) / dU[i];
+        }
+        else
+        {
+            abortt = true;
+            return x;
+        }
+    }
+    return x;
+}
+
+void reconstructLU(const vector<double> &L, const vector<double> &U, const vector<double> &dU, int n)
+{
+    cout << "LU Matrix:\n";
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << getL(L, i, j, n) * getU(U, dU, j, j, n) + getU(U, dU, i, j, n) << " ";
+        }
+        cout << endl;
+    }
+}
+
 void run(vector<vector<double>> &A, vector<double> &dU, vector<double> &b, int n, double epsilon)
 {
     const vector<vector<double>> Ainit = A;
@@ -237,6 +357,33 @@ void run(vector<vector<double>> &A, vector<double> &dU, vector<double> &b, int n
     calculNorma(Ainit, xLU, b, n);
 
     calculEigen(Ainit, b, xLU, n);
+
+    // Bonus:
+    vector<double> L(n * (n + 1) / 2, 0.0);
+    vector<double> U(n * (n + 1) / 2, 0.0);
+
+    LUdecomposition(Ainit, L, U, dU, n, epsilon);
+    if (abortt == true)
+    {
+        return;
+    }
+    vector<double> y = solveLyB(L, b, n, epsilon);
+    if (abortt == true)
+    {
+        return;
+    }
+    vector<double> x = solveUxY(U, dU, y, n, epsilon);
+    if (abortt == true)
+    {
+        return;
+    }
+
+    cout << "Solution x: ";
+    for (double val : x)
+        cout << val << " ";
+    cout << endl;
+
+    reconstructLU(L, U, dU, n);
 }
 
 int main()
@@ -248,38 +395,45 @@ int main()
     srand(time(0));
 
     // Create a 100x100 sparse matrix (mostly zeros, with random values on the diagonal and some random off-diagonal values)
-    vector<vector<double>> A(n, vector<double>(n, 0));  // Initialize a 100x100 matrix with zeros
+    vector<vector<double>> A(n, vector<double>(n, 0)); // Initialize a 100x100 matrix with zeros
 
     // Fill the diagonal with non-zero values
-    for (int i = 0; i < n; i++) {
-        A[i][i] = (rand() % 100 + 1);  // Assign a random non-zero value to the diagonal
+    for (int i = 0; i < n; i++)
+    {
+        A[i][i] = (rand() % 100 + 1); // Assign a random non-zero value to the diagonal
     }
 
     // Optionally fill some off-diagonal values to make the matrix sparse
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            if (i != j && rand() % 5 == 0) {  // Add non-zero elements with a certain probability
-                A[i][j] = rand() % 10 + 1;  // Random off-diagonal value
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (i != j && rand() % 5 == 0)
+            {                              // Add non-zero elements with a certain probability
+                A[i][j] = rand() % 10 + 1; // Random off-diagonal value
             }
         }
     }
 
     // Create vector b with random values
     vector<double> b(n);
-    for (int i = 0; i < n; i++) {
-        b[i] = rand() % 100 + 1;  // Random values for vector b
+    for (int i = 0; i < n; i++)
+    {
+        b[i] = rand() % 100 + 1; // Random values for vector b
     }
 
     // Create vector dU with ones (or you can customize it)
-    //vector<double> dU(n, 1);  // Initialize vector dU with 1's (you can adjust this if necessary)
+    // vector<double> dU(n, 1);  // Initialize vector dU with 1's (you can adjust this if necessary)
     vector<double> dU(n);
-    for (int i = 0; i < n; i++) {
-        dU[i] = rand() % 100 + 1;  // Random values for vector b
+    for (int i = 0; i < n; i++)
+    {
+        dU[i] = rand() % 100 + 1; // Random values for vector b
     }
 
     // Save to file "input.txt"
     ofstream outputFile("output.txt");
-    if (!outputFile) {
+    if (!outputFile)
+    {
         cerr << "Failed to open the file!" << endl;
         return 1;
     }
@@ -289,28 +443,31 @@ int main()
     outputFile << epsilon << endl;
 
     // Write matrix A to file
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
             outputFile << A[i][j] << " ";
         }
         outputFile << endl;
     }
 
     // Write vector b to file
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         outputFile << b[i] << " ";
     }
     outputFile << endl;
 
     // Write vector dU to file
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         outputFile << dU[i] << " ";
     }
     outputFile << endl;
 
     // Close the file
     outputFile.close();
-
 
     run(A, dU, b, n, epsilon);
 
